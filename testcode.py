6155b7b3bbd52e09ada7ef40b2365bef9f69c179
Phillips <phillips.1622@ast-nc211563.home>
2019-11-12 00:24:36 -0500

    Test Code I Made for Numerical Methods

---
+++ testcode.py
@@ -0,0 +1,5 @@
+
+def do_something(num):
+    a=num+1
+    #return a
+    print('This is the number Whoot! Whoot!:',a) #this will make it much easier in future problems to see that something is actually happening
